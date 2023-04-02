d1949e800a31cd11c8cdb4c0c3ba0fdd3bd111ba
Suz033 <susanamy2324@gmail.com>
2023-04-02 16:58:52 +0800

    feat: add edit function

--- app.js
+++ app.js
@@ -45,8 +45,11 @@ app.get('/', (req, res) => {
 })
 
 app.get('/restaurants/:id', (req, res) => {
-  const list = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.id)
-  res.render('show', { list: list })
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then(restaurants => res.render('show', { restaurants }))
+    .catch(error => console.log(error))
 })
 
 app.get('/search', (req, res) => {
@@ -56,6 +59,40 @@ app.get('/search', (req, res) => {
     return list.name.match(re) || list.category.match(re)
   })
   res.render('index', { keyword: keyword, list: list})
+})
+
+app.get('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then(restaurants => res.render('edit', { restaurants }))
+    .catch(error => console.log(error))
+})
+
+app.post('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+  const name_en = req.body.name_en
+  const category = req.body.category
+  const description = req.body.description
+  const image = req.body.image
+  const location = req.body.location
+  const phone = req.body.phone
+  const google_map = req.body.google_map
+  return Restaurant.findById(id)
+    .then(restaurants => {
+      restaurants.name = name
+      restaurants.name_en = name_en
+      restaurants.category = category
+      restaurants.description = description
+      restaurants.image = image
+      restaurants.location = location
+      restaurants.phone = phone
+      restaurants.google_map = google_map
+      return restaurants.save()
+    })
+    .then(() => res.redirect(`/restaurants/${id}`))
+    .catch(error => console.log(error))
 })
 
 
