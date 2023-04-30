5deaab3723a1417e711046ca12d933328fb73447
Suz033 <susanamy2324@gmail.com>
2023-04-30 22:43:35 +0800

    feat: add isAuthenticated to show different view

--- app.js
+++ app.js
@@ -37,6 +37,15 @@ require('./config/mongoose')
 app.use(express.static('public'))
 
 
+//// auth state ////
+app.use((req, res, next) => {
+  // 你可以在這裡 console.log(req.user) 等資訊來觀察
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
+
+
 //// routes setting ////
 const routes = require('./routes')
 app.use(routes)
