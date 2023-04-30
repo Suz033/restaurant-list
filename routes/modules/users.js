7353e1bf85a52d13d047878ebd39f1bba67d663d
Suz033 <susanamy2324@gmail.com>
2023-04-30 20:15:40 +0800

    feat: add register page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -11,5 +11,36 @@ router.get('/login', (req, res) => {
 router.post('/login', (req, res) => {
 })
 
+// register
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+
+  User.findOne({ email })
+    .then(user => {
+      if (user) {
+        console.log('User already exists.')
+        res.render('register', {
+          name,
+          email,
+          password,
+          confirmPassword
+        })
+      } else {
+        return User.create({
+          name,
+          email,
+          password
+        })
+          .then(() => res.redirect('/'))
+          .catch(err => console.error(err))
+      }
+    })
+    .catch(err => console.log(err))
+})
+
 // exports
 module.exports = router
