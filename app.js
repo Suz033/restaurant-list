36561adf4c804bc3a3460aef91807c5170a1bb09
Suz033 <susanamy2324@gmail.com>
2023-05-02 16:22:14 +0800

    feat: add isAuthenticated to show different view

--- app.js
+++ app.js
@@ -32,6 +32,14 @@ app.use(methodOverride('_method'))
 usePassport(app)
 
 
+//// auth state ////
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
+
+
 //// db ////
 require('./config/mongoose')
 
