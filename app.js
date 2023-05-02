938213e4812b3065d70cdad3c5877caa4af218e8
Suz033 <susanamy2324@gmail.com>
2023-05-02 14:35:58 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 //// modules ////
-// express
 const express = require('express')
 const app = express()
-
-// handlebars
 const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
+const session = require('express-session')
+
+// hbs
 app.engine('hbs', exphbs ({ defaultLayout: 'main', extname: 'hbs'}))
 app.set('view engine', 'hbs')
+
+// express-session
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
+
+// hbs-helpers
 const helpers = require('handlebars-helpers')()
 
 // body-parser (before setting route)
 app.use(express.urlencoded({ extended: true }))
 
 // method-override
-const methodOverride = require('method-override')
 app.use(methodOverride('_method'))
 
 
